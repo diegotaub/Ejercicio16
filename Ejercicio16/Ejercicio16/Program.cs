8306af75dae36125f945d2768a7c3426c64df00a
diego.taub <diego.taub@accenture.com>
2021-03-21 21:12:52 -0300

    Ejercicio16

---
+++ Ejercicio16/Ejercicio16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un valor: ");
+            if(Int32.TryParse(Console.ReadLine(), out int valor))
+            {
+                Console.Write("El valor puede interpretarse como entero");
+            }
+            else
+            {
+                Console.Write("El valor no puede interpretarse como entero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
